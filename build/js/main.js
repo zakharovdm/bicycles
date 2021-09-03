d9f3ab61b329d42f5f96848c2467e16f82634760
Dmitry Zakharov <dzaharov794@gmail.com>
2021-09-03 17:16:31 +0300

    Добавляет плавный скролл для перехода по якорям

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
-var siteNav = document.querySelector('.site-nav');
-var navToggle = siteNav.querySelector('.site-nav__toggle');
+const siteNav = document.querySelector('.site-nav');
+const navToggle = siteNav.querySelector('.site-nav__toggle');
+const links = document.querySelectorAll('a[href^="#"]');
 
 // Menu
 
@@ -10,3 +11,16 @@ navToggle.addEventListener('click', function () {
   siteNav.classList.toggle('site-nav--closed');
   siteNav.classList.toggle('site-nav--opened');
 });
+
+//SmoothScroll
+
+for (let link of links) {
+  link.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      const id = link.getAttribute('href');
+
+      document.querySelector(id).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+};
