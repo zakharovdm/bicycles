7bfd61e4361ccd201c10c63759d2b30e65a01cbf
Dmitry Zakharov <dzaharov794@gmail.com>
2021-09-10 20:40:31 +0300

    Добавляет полифил для плавного скролла

--- source/js/main.js
+++ source/js/main.js
@@ -1,78 +1,83 @@
 'use strict';
-const siteNav = document.querySelector('.site-nav');
-const header = document.querySelector('.site-header');
-const navToggle = siteNav.querySelector('.site-nav__toggle');
-const links = document.querySelectorAll('a[href^="#"]');
-const form = document.querySelector('.form');
-const inputs = form.querySelectorAll('input');
-const submit = form.querySelector('button');
-const inputPhone = form.querySelector('input[name=phone]');
-const regularPhone =  /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+(function () {
+  import smoothscroll from 'smoothscroll-polyfill';
 
-//SmoothScroll
+  const siteNav = document.querySelector('.site-nav');
+  const header = document.querySelector('.site-header');
+  const navToggle = siteNav.querySelector('.site-nav__toggle');
+  const links = document.querySelectorAll('a[href^="#"]');
+  const form = document.querySelector('form');
+  const inputs = form.querySelectorAll('input');
+  const submit = form.querySelector('button');
+  const inputPhone = form.querySelector('input[name=phone]');
+  const regularPhone =  /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
 
-for (let link of links) {
-  link.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      const id = link.getAttribute('href');
+  //SmoothScroll
 
-      document.querySelector(id).scrollIntoView({
-          behavior: 'smooth'
-      });
-  });
-};
+  smoothscroll.polyfill();
+  for (let link of links) {
+    link.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        const id = link.getAttribute('href');
 
-// Menu
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  };
 
-header.classList.remove('site-header--nojs');
+  // Menu
 
-const toggleMenu = () => {
-  siteNav.classList.toggle('site-nav--closed');
-  siteNav.classList.toggle('site-nav--opened')
+  header.classList.remove('site-header--nojs');
 
-  if(siteNav.classList.contains('site-nav--closed')){
-    bodyUnfixPosition();
-  } else {
-    bodyFixPosition();
-  }
-};
+  const toggleMenu = () => {
+    siteNav.classList.toggle('site-nav--closed');
+    siteNav.classList.toggle('site-nav--opened')
 
-navToggle.addEventListener('click', toggleMenu, {passive: true});
-
-function bodyFixPosition() {
-  setTimeout( () => {
-    if ( !document.body.hasAttribute('data-body-scroll-fix') ) {
-      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-      document.body.setAttribute('data-body-scroll-fix', scrollPosition);
-      document.body.style.overflow = 'hidden';
-      document.body.style.position = 'fixed';
-      document.body.style.top = `-${  scrollPosition  }px`;
-      document.body.style.left = '0';
-      document.body.style.width = '100%';
+    if(siteNav.classList.contains('site-nav--closed')){
+      bodyUnfixPosition();
+    } else {
+      bodyFixPosition();
     }
-  }, 15 );
-}
+  };
 
-function bodyUnfixPosition() {
-  if ( document.body.hasAttribute('data-body-scroll-fix') ) {
-    const scrollPosition = document.body.getAttribute('data-body-scroll-fix');
-    document.body.removeAttribute('data-body-scroll-fix');
-    document.body.style.overflow = '';
-    document.body.style.position = '';
-    document.body.style.top = '';
-    document.body.style.left = '';
-    document.body.style.width = '';
-    window.scroll(0, scrollPosition);
+  navToggle.addEventListener('click', toggleMenu, {passive: true});
+
+  function bodyFixPosition() {
+    setTimeout( () => {
+      if ( !document.body.hasAttribute('data-body-scroll-fix') ) {
+        const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+        document.body.setAttribute('data-body-scroll-fix', scrollPosition);
+        document.body.style.overflow = 'hidden';
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${  scrollPosition  }px`;
+        document.body.style.left = '0';
+        document.body.style.width = '100%';
+      }
+    }, 15 );
   }
-}
 
-// Validation
+  function bodyUnfixPosition() {
+    if ( document.body.hasAttribute('data-body-scroll-fix') ) {
+      const scrollPosition = document.body.getAttribute('data-body-scroll-fix');
+      document.body.removeAttribute('data-body-scroll-fix');
+      document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.left = '';
+      document.body.style.width = '';
+      window.scroll(0, scrollPosition);
+    }
+  }
 
-inputPhone.addEventListener('input', () => {
-  if (!regularPhone.test(inputPhone.value)) {
-    inputPhone.setCustomValidity('Введите корректный номер телефона');
-    inputPhone.classList.add('form-inner__input--error');
-  } else {inputPhone.setCustomValidity('');
-  inputPhone.classList.remove('form-inner__input--error');}
-  inputPhone.reportValidity();
-})
+  // Validation
+
+  inputPhone.addEventListener('input', () => {
+    if (!regularPhone.test(inputPhone.value)) {
+      inputPhone.setCustomValidity('Введите корректный номер телефона');
+      inputPhone.classList.add('form-inner__input--error');
+    } else {inputPhone.setCustomValidity('');
+    inputPhone.classList.remove('form-inner__input--error');}
+    inputPhone.reportValidity();
+  })
+})();
